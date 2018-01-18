287bd7fd49dfff7e1a8615bab7cc264b326c94fd
Michael Allar <allar@michaelallar.com>
2018-01-18 01:12:34 -0800

    Removed Bindings dep

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('bindings')('throestlhash.node')
+var myModule
+
+if (process.env.DEBUG) {
+  myModule= require('./build/Debug/throestlhash.node')
+} else {
+  myModule= require('./build/Release/throestlhash.node')
+}
+
+module.exports = myModule
